82e9a441001cfe791ec9700dcd360a85af3073b3
watcharaphat <watcharaphat.3166@gmail.com>
2017-04-26 14:18:18 +0700

    store tweets into mongodb instead of json

--- collect_twitter.py
+++ collect_twitter.py
@@ -1,20 +1,27 @@
 #Import the necessary methods from tweepy library
 from api_keys import *
+from mongo_auth import *
 from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
 import json
+from pymongo import MongoClient
 
 #This is a basic listener that just prints received tweets to stdout.
 class MyListener(StreamListener):
 
     def on_data(self, data):
         try:
-            with open('tweets.json', 'a') as f:
-                f.write(data)
-                dat = json.loads(data)
-                print "%s %s" % (dat['created_at'], dat['text'])
-                return True
+            tweet = json.loads(data)
+            print "%s %s" % (tweet['created_at'], tweet['text'])
+            client = MongoClient('mongodb://' + MONGO_USERNAME + ':'
+                                 + MONGO_PASSWORD + '@watcharaphat.com')
+            db = client['twitter_db']
+            collection = db['twitter_collection']
+            collection.insert(tweet)
+
+            return True
+
         except BaseException as e:
             print("--> Error on_data: %s" % str(e))
             pass
