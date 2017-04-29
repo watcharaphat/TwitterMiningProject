6b9b5ad8578bc8c40815b38f9edbaef1a899dc5d
watcharaphat <watcharaphat.3166@gmail.com>
2017-04-29 21:13:09 +0000

    add the program to collect good tweets

--- good_tweets.py
+++ good_tweets.py
@@ -5,6 +5,7 @@ from tweepy import OAuthHandler
 from tweepy import Stream
 import json
 from pymongo import MongoClient
+from bson.json_util import dumps
 
 #This is a basic listener that just prints received tweets to stdout.
 class MyListener(StreamListener):
@@ -12,14 +13,21 @@ class MyListener(StreamListener):
     def on_data(self, data):
         try:
             tweet = json.loads(data)
-            print "%s %s" % (tweet['created_at'], tweet['text'])
-            collection.insert(
-                {
-                    "text": tweet['text']
-                }
-            )
 
-            return True
+            if not tweet['text'].startswith('RT'):
+                print "%s %s" % (tweet['created_at'], tweet['text'])
+                collection.insert(
+                    {
+                        "user": {
+                            "screen_name": tweet['user']['screen_name'],
+                            "id_str": tweet['user']['id_str']
+                        },
+                        "time": tweet['created_at'],
+                        "text": tweet['text']
+                    }
+                )
+
+                return True
 
         except BaseException as e:
             print("--> Error on_data: %s" % str(e))
@@ -39,12 +47,15 @@ if __name__ == '__main__':
     client = MongoClient('mongodb://' + MONGO_USERNAME + ':' + MONGO_PASSWORD
                          + '@watcharaphat.com')
     db = client['twitter_db']
-    collection = db['twitter_train']
+    collection = db['good_tweets']
+
+    with open('keyword/good_words.txt') as f:
+        content = f.readlines()
+    good_words = [line.decode('utf-8').strip('\n') for line in content]
+    #good_words = [x.strip('\n') for x in content]
+    # good_words = dumps(content)
 
     #This line filter Twitter Streams to capture data by the keywords: '
-    twitter_stream.filter(languages=["en"],
-                          track=['porn', 'sex', 'cock', 'dick', 'fuck',
-                                 'blowjob', 'handjob', 'pussy', 'MILF',
-                                 'gangbang', '#porn', '#sex', '#cock', '#dick',
-                                 '#fuck', '#blowjob', "#handjob", '#pussy',
-                                 '#MILF', '#gangbang'])
+    twitter_stream.filter(languages = ["en"],
+                          track = good_words
+                          )
