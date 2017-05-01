02cf8abc2c443305d6201a29364aa5d44c6401d9
watcharaphat <watcharaphat.3166@gmail.com>
2017-05-01 17:03:39 +0700

    add train_data_generator

---
+++ tt.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+from textblob.classifiers import NaiveBayesClassifier
+from nltk.corpus import stopwords
+
+stopset = set(stopwords.words('english'))
+ 
+def stopword_filtered_word_feats(words):
+    return dict([(word, True) for word in words if word not in stopset])
+ 
+evaluate_classifier(stopword_filtered_word_feats)
+
+
+train = [
+    ('I love this sandwich.', 'pos'),
+    ('This is an amazing place!', 'pos'),
+    ('I feel very good about these beers.', 'pos'),
+    ('This is my best work.', 'pos'),
+    ("What an awesome view", 'pos'),
+    ('I do not like this restaurant', 'neg'),
+    ('I am tired of this stuff.', 'neg'),
+    ("I can't deal with this", 'neg'),
+    ('He is my sworn enemy!', 'neg'),
+    ('My boss is horrible.', 'neg')
+]
+test = [
+    ('The beer was good.', 'pos'),
+    ('I do not enjoy my job', 'neg'),
+    ("I ain't feeling dandy today.", 'neg'),
+    ("I feel amazing!", 'pos'),
+    ('Gary is a friend of mine.', 'pos'),
+    ("I can't believe I'm doing this.", 'neg')
+]
+
+for item in train:
+    print type(item)
+
+cl = NaiveBayesClassifier(train)
+print cl.accuracy(test)
+print cl.show_informative_features()
